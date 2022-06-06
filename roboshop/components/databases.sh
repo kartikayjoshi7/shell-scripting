f3e041fbafcc13f75cd2795906bf021ddb1661f4
kartikayjoshi7 <95585794+kartikayjoshi7@users.noreply.github.com>
2022-06-06 06:44:13 +0530

    Update databases.sh

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -30,7 +30,7 @@ echo -e "        ------>>>>>> \e[1;35mRedis Setup\e[0m <<<<<<------"
 curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
 STAT_CHECK $? "Download Redis Repo"
 
-yum install redis -y  &>>${LOG_FILE}
+yum install redis-6.2.7 -y  &>>${LOG_FILE}
 STAT_CHECK $? "Install Redis"
 
 
