aab897d032991a87c5173d0b1bc5f2926820ee75
Kartikay <kartikayjoshiprivate@gmail.com>
2022-06-06 06:49:20 +0530

    Checking

--- 01-simple.sh
+++ 01-simple.sh
@@ -8,4 +8,4 @@ ls
 ## TO execute the script we can execute with
 # bash script or sh script
 
-# In Redhat family , BASH and SH are same. IN other OS like Ubuntu those are different.
+# In Redhat family , BASH and SH are same . IN other OS like Ubuntu those are different.
